a1d7cef65dddf63c22851a4400165314f69928e0
Marcos Diaz <ma.nahuel.d@gmail.com>
2025-05-19 02:31:09 -0300

    Creación de la clase abstracta Muestra

---
+++ src/unq/integrador/Muestra.java
@@ -0,0 +1,52 @@
+package unq.integrador;
+
+import java.util.ArrayList;
+
+/**
+ * Esta es la clase abstracta de Muestra, desde esta heredan
+ * MuestraLibre, MuestraExperto y MuestraCerrada.
+ * Conoce a su usuario para poder pasar de tipo de muestra
+ * La fotografía es un String porque no se me ocurrió un tipo adecuado para eso
+ * La ubicación es un String MOMENTÁNEAMENTE porque la implementación de Ubicación todavía no es mi sección
+ * También tiene una ArrayList de opiniones (Un Enum), que contiene todas las opiniones de usuarios
+ * 
+ * Aunque estoy pensando en cambiarlo en un HashMap para beneficiarme de la cantidad usandola como valor y
+ * las claves como tipo de opinión, así quedan mejor addOpinion y resultado actual
+ * 
+ * @author Díaz Marcos, Mendoza Tomás, Monteros Dario
+ */
+public abstract class Muestra implements IMuestra {
+    protected Usuario user;
+    protected String fotografia;
+    protected String ubicacion;
+    protected ArrayList<Opinion> opiniones;
+    
+    /**
+     * Constructor de la clase Abstracta Muestra
+     * @param user usuario que publicó la muestra
+     * @param fotografia fotografía del usuario que publicó la muestra
+     * @param ubicacion ubicación del usuario que publicó la muestra
+     */
+    public Muestra(Usuario user, String fotografia, String ubicacion) {
+        this.user = user;
+        this.fotografia = fotografia;
+        this.ubicacion = ubicacion;
+        this.opiniones = new ArrayList<Opinion>();
+    }
+
+    /**
+     * Método para conseguir el resultado actual de opiniones,
+     * en esta clase es abstracto porque cada tipo de muestra lo calcula distinto
+     * 
+     * @return La opinión con más votos, que sería la que más 
+     * ocurrencias tiene en la lista de opiniones de la clase
+     */
+    public abstract Opinion resultadoActual();
+
+    /**
+     * Agregua una opinión a la lista.
+     * 
+     * @param op una opinión para agregar a la lista
+     */
+    public abstract void addOpinion(Opinion op, Usuario user);
+}

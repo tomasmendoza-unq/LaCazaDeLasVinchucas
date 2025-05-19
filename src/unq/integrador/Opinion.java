349ed9180861a7eb869765a4da5776d0e46236b0
Marcos Diaz <ma.nahuel.d@gmail.com>
2025-05-19 02:31:25 -0300

    Creación del Enum Opinion

---
+++ src/unq/integrador/Opinion.java
@@ -0,0 +1,14 @@
+package unq.integrador;
+
+/**
+ * Enum de opiniones
+ */
+public enum Opinion {
+    VINCHUCA_INFESTANS,
+    VINCHUCA_SORDIDA,
+    VINCHUCA_GUASAYANA,
+    CHINCHA_FOLIADA,
+    PHTIA_CHINCHE,
+    NINGUNA,
+    IMAGEN_POCO_CLARA
+}

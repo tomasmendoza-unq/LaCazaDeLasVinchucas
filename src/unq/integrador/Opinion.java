0e286fdf2a52f07b07562f4a36e06801acd5f68c
Marcos Diaz <ma.nahuel.d@gmail.com>
2025-06-12 02:21:05 -0300

    se cambia el enum Opinion, por una clase concreta y se crea un enum TipoOpinion para esa clase

--- src/unq/integrador/Opinion.java
+++ src/unq/integrador/Opinion.java
@@ -1,20 +1,25 @@
 package unq.integrador;
 
-/**
- * Enum de opiniones
- */
-public enum Opinion {
-    VINCHUCA_INFESTANS,
-    VINCHUCA_SORDIDA,
-    VINCHUCA_GUASAYANA,
-    CHINCHA_FOLIADA,
-    PHTIA_CHINCHE,
-    NINGUNA,
-    IMAGEN_POCO_CLARA
-}
+import java.time.LocalDate;
 
-/**
- * Cambiar este enum por una clase que tenga como atributos
- * tipo, que recibe un enum de TiposDeOpiniones
- * fecha, que es un localDate
- */
+public class Opinion {
+    private TipoOpinion tipo;
+    private LocalDate fechaDeCreacion;
+    
+    public Opinion(TipoOpinion tipo) {
+        this.tipo = tipo;
+        this.fechaDeCreacion = LocalDate.now();
+    }
+
+    public TipoOpinion getTipo() {
+        return this.tipo;
+    }
+    
+    public String imprimirTipo() {
+        return tipo.imprimirTipo();
+    }
+
+    public LocalDate getFechaDeCreacion() {
+        return this.fechaDeCreacion;
+    }
+}
---
+++ src/unq/integrador/TipoOpinion.java
@@ -0,0 +1,47 @@
+package unq.integrador;
+
+public enum TipoOpinion {
+    VINCHUCA_INFESTANS(){
+      @Override
+      public String imprimirTipo(){
+        return "Vinchuca Infestans";
+      }
+    },
+    VINCHUCA_SORDIDA(){
+      @Override
+      public String imprimirTipo(){
+        return "Vinchuca Sordida";
+      }
+    },
+    VINCHUCA_GUASAYANA(){
+      @Override
+      public String imprimirTipo(){
+        return "Vinchuca Guasayana";
+      }
+    },
+    CHINCHA_FOLIADA(){
+      @Override
+      public String imprimirTipo(){
+        return "Chincha Foliada";
+      }
+    },
+    PHTIA_CHINCHE(){
+      @Override
+      public String imprimirTipo(){
+        return "Phtia Chinche";
+      }
+    },
+    NINGUNA(){
+      @Override
+      public String imprimirTipo(){
+        return "Ninguna";
+      }
+    },
+    IMAGEN_POCO_CLARA(){
+      @Override
+      public String imprimirTipo(){
+        return "Imagen poco clara";
+      }
+    };
+    public abstract String imprimirTipo();
+}

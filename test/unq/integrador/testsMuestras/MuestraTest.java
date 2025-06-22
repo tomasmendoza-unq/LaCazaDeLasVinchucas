0fa1b4cf231a86e822dfa2568fe4eceb8f4da495
Ruben Dario Monteros <monterosrubendario@gmail.com>
2025-06-22 02:16:19 -0300

    Se agregan test para el resultado actual y la fecha de última votación de la clase Muestra

--- test/unq/integrador/testsMuestras/MuestraTest.java
+++ test/unq/integrador/testsMuestras/MuestraTest.java
@@ -8,6 +8,7 @@ import java.time.LocalDate;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import unq.integrador.*;
+import unq.integrador.enums.TipoOpinion;
 import unq.integrador.impls.Muestra;
 import unq.integrador.impls.Opinion;
 
@@ -25,10 +26,44 @@ public class MuestraTest {
 
     @Test
     public void testGetters() {
+        Opinion op = mock(Opinion.class);
+        when(op.getFechaDeCreacion()).thenReturn(LocalDate.now());
+
         assertEquals(1, muestra.getIDUsuario());
         assertEquals("Foto", muestra.getFotografia());
         assertEquals(ubicacion, muestra.getUbicacion());
         assertEquals(LocalDate.now(), muestra.getFechaCreacion());
+
+        muestra.agregarOpinionBasico(op);
+        
+        assertEquals(LocalDate.now(), muestra.getFechaUltimaVotacion());
+    }
+
+    @Test
+    public void testResultadoActual() {
+        // Mock de la opinion
+        Opinion op = mock(Opinion.class);
+        when(op.imprimirTipo()).thenReturn("Ninguna");
+        when(op.getTipo()).thenReturn(TipoOpinion.NINGUNA);
+        when(op.getFechaDeCreacion()).thenReturn(LocalDate.now());
+
+        // Antes de agregar la opinión no hay resultado
+        assertEquals("No definido", muestra.resultadoActual());
+
+        // Al agregar la opinión el resultado cambia
+        muestra.agregarOpinionBasico(op);
+        assertEquals("Ninguna", muestra.resultadoActual());
+
+        // Se cambia el tipo de la opinión
+        when(op.imprimirTipo()).thenReturn("Imagen poco clara");
+        when(op.getTipo()).thenReturn(TipoOpinion.IMAGEN_POCO_CLARA);
+
+        // Al agregar más opiniones de otro tipo el resultado vuelve a cambiar
+        muestra.agregarOpinionBasico(op);
+        muestra.agregarOpinionBasico(op);
+        assertEquals("Imagen poco clara", muestra.resultadoActual());
+
+
     }
 
     @Test
@@ -42,6 +77,12 @@ public class MuestraTest {
         assertEquals(
             "Usuario 1 opinó: Ninguna, en la fecha: " + LocalDate.now().toString() + ", con categoría: Básico", 
             muestra.verRegistroNro(1));
+        
+        muestra.agregarOpinionExperto(op);
+
+        assertEquals(
+            "Usuario 1 opinó: Ninguna, en la fecha: " + LocalDate.now().toString() + ", con categoría: Experto", 
+            muestra.verRegistroNro(2));
     }
 
     @Test
